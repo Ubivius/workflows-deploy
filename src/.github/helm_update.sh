003a1eb43cbf540944ecc33eb419dafe038b5a12
ysma500 <ysmael.fortier@gmail.com>
2021-10-29 11:06:56 -0400

    Add cert-manager refs and a custom cmd for mongodb

--- src/.github/helm_update.sh
+++ src/.github/helm_update.sh
@@ -14,9 +14,10 @@ case $1 in
     ;;
     mongodb)
         helm upgrade mongodb --install --version 10.26.0 bitnami/mongodb -f chart/values.yaml
+        kubectl apply -f chart/configmap.yaml
     ;;
-    pebble)
-        helm upgrade pebble --install --version 0.2.2 jupyterhub/pebble -f chart/values.yaml
+    cert-manager)
+        helm upgrade cert-manager --install --version 1.6.0 jetstack/cert-manager -f chart/values.yaml
     ;;
     traefik)
         helm upgrade traefik --install --version 10.3.4 traefik/traefik -f values.yaml
--- src/.github/tools_install.sh
+++ src/.github/tools_install.sh
@@ -12,7 +12,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 # For Traefik
 helm repo add traefik https://helm.traefik.io/traefik
 # For Pebble
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
+helm repo add jetstack https://charts.jetstack.io
 # For Agones
 helm repo add agones https://agones.dev/chart/stable
 helm repo update
