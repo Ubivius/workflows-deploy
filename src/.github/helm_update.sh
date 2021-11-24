0c7c7edf81bb171cc7dbe457558fd3c399027dd5
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-11-24 16:26:32 -0500

    update helm_update script

--- src/.github/helm_update.sh
+++ src/.github/helm_update.sh
@@ -17,11 +17,13 @@ case $1 in
         kubectl apply -f chart/configmap.yaml
     ;;
     cert-manager)
-        helm upgrade cert-manager --install --version 1.6.0 jetstack/cert-manager -f chart/values.yaml
+        helm upgrade cert-manager --install --version 1.6.0 jetstack/cert-manager --namespace cert-manager --create-namespace --set installCRDs=true
+        kubectl apply -f chart/cluster-issuer-ss.yaml
+        kubectl apply -f chart/cm-certificates-ss.yaml
     ;;
     traefik)
         helm upgrade traefik --install --version 10.3.4 traefik/traefik -f chart/values.yaml
-        kubectl apply -f chart/dashboard.yaml
+        kubectl apply -f chart/ingressroute.yaml
         kubectl apply -f chart/middleware.yaml
     ;;
     *)
