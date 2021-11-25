cd3bd1b38defa392c32e71a0c696db1fa30a51b0
ysma500 <ysmael.fortier@gmail.com>
2021-11-25 13:02:55 -0500

    Added required deploy steps

--- src/.github/helm_update.sh
+++ src/.github/helm_update.sh
@@ -2,18 +2,23 @@
 case $1 in
     agones)
         helm upgrade agones --install --version 1.17.0 --namespace agones-system --create-namespace agones/agones
+        kubectl apply -f chart/fleet.yaml
+        kubectl apply -f chart/gameservers-role.yaml
+        kubectl create rolebinding gameservers-reader-pod --role=gameservers-reader --serviceaccount=default:default
     ;;
     elasticsearch)
         helm upgrade elasticsearch --install --version 7.14.0 elastic/elasticsearch -f chart/values.yaml
     ;;
     keycloak)
         helm upgrade keycloak --install --version 5.1.0 bitnami/keycloak -f chart/values.yaml
+        kubectl apply -f chart/ingressroute.yaml
     ;;
     kibana)
         helm upgrade kibana --install --version 7.14.0 elastic/kibana -f chart/values.yaml
     ;;
     mongodb)
         helm upgrade mongodb --install --version 10.26.0 bitnami/mongodb -f chart/values.yaml
+        kubectl apply -f chart/ingressroute.yaml
         kubectl apply -f chart/configmap.yaml
     ;;
     cert-manager)
